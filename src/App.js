6f956fa82e20c8ffbd351001dcfe5018ffcc7c18
Karlo Šimunović <karlo.simunovic@gmail.com>
2024-11-26 15:49:49 +0100

    Add support for touch devices and enhance theme persistence.
    - Added `react-dnd-touch-backend` to handle touch interactions.
    - Updated `DndProvider` in `App` to select backend based on touch capability.
    - Improved theme persistence by storing the selected theme in `localStorage`.
    - Removed unused styles from `SearchBar`.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend'; // Correct import
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 import { ThemeProvider } from './contexts/ThemeContext';
 import MainComponent from './components/MainComponent/MainComponent';
 import Navigation from './components/Navigation/Navigation';
 import './styles/App.scss';
 
+const isTouchDevice = 'ontouchstart' in window;
+
 function App() {
     return (
-        <DndProvider backend={HTML5Backend}>
+        <DndProvider backend={isTouchDevice ? TouchBackend : HTML5Backend}>
             <ThemeProvider>
                 <MainComponent />
                 <Navigation />
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,17 +1,18 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import './ThemeContext.scss';
 
-// Create context
 export const ThemeContext = createContext();
 
-// Custom hook for using theme context
 export const useTheme = () => {
     return useContext(ThemeContext);
 };
 
-// Theme provider component
 export const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState('light'); // 'light' or 'dark'
+    const [theme, setTheme] = useState(() => localStorage.getItem('theme') || 'light');
+
+    useEffect(() => {
+        localStorage.setItem('theme', theme);
+    }, [theme]);
 
     const toggleTheme = () => {
         setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
